6e2c28d56ee78538426d5ad662f0f5a6155f7004
ZeroSTF <kateskatous@gmail.com>
2024-09-30 21:10:28 +0100

    cors origin fix

--- src/main/java/tn/zeros/marketmaster/config/CorsConfig.java
+++ src/main/java/tn/zeros/marketmaster/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Adjust as needed
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // Adjust as needed
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
