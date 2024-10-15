32864df0219fe6b4a655b56e3d44f5380eff34c5
ZeroSTF <kateskatous@gmail.com>
2024-10-15 13:31:50 +0100

    added auditing configuration

---
+++ src/main/java/tn/zeros/marketmaster/entity/common/Auditable.java
@@ -0,0 +1,32 @@
+package tn.zeros.marketmaster.entity.common;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public abstract class Auditable {
+
+    @CreatedBy
+    private String createdBy;
+
+    @CreatedDate
+    private Instant createdDate;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    private Instant lastModifiedDate;
+}
