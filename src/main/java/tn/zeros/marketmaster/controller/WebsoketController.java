f8c27676730a55b936b002dab159082cc9c9b3bd
gaddour77 <mohamedabdelkader.benfraj@esprit.tn>
2024-10-19 19:50:49 +0100

    yahoo finance

--- src/main/java/tn/zeros/marketmaster/controller/WebsoketController.java
+++ src/main/java/tn/zeros/marketmaster/controller/WebsoketController.java
@@ -1,28 +1,40 @@
 package tn.zeros.marketmaster.controller;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
+import tn.zeros.marketmaster.dto.AssetDailyDto;
+import tn.zeros.marketmaster.service.AlphaVantageService;
+import tn.zeros.marketmaster.service.YfinaceService;
+
+import java.util.List;
 
 @Controller
-@AllArgsConstructor
-
+@RequiredArgsConstructor
 public class WebsoketController {
+    @Value("${SERVER_PORT}")
+    private String serverport;
+
     private final SimpMessagingTemplate messagingTemplate;
+
     private final RestTemplate restTemplate;
 
 
+    private final YfinaceService yfinaceService;
 
     @MessageMapping("/fetchStockData") // WebSocket endpoint to fetch stock data
-    public void fetchStockData(String symbol) {
+    public void fetchStockData() {
         // Call the REST endpoint to get stock data
-        String stockData = restTemplate.getForObject("http://localhost:8081/alpha/stock-data?symbol=" + symbol, String.class);
+        List<AssetDailyDto> dailyDtos = yfinaceService.dailyDtos();
 
         // Send the retrieved stock data to all subscribed clients
-        messagingTemplate.convertAndSend("/topic/market", stockData);
+        messagingTemplate.convertAndSend("/topic/market", dailyDtos);
     }
+
 }
