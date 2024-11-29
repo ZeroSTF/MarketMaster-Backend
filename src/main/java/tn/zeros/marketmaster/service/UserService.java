e783197a03890eafb3a41750a252312df5590aa5
ZeroSTF <35609287+ZeroSTF@users.noreply.github.com>
2024-11-29 14:41:56 +0100

    Update UserService.java

--- src/main/java/tn/zeros/marketmaster/service/UserService.java
+++ src/main/java/tn/zeros/marketmaster/service/UserService.java
@@ -41,7 +41,9 @@ public class UserService {
                 .orElseThrow(() -> new UserNotFoundException("User not found: " + username));
         return UserDTO.fromEntity(user);
     }
+
     public User getUserByUseranme(String username){
-        return userRepository.findByUsername(username).orElse(null);
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException("User not found: " + username));
     }
 }
