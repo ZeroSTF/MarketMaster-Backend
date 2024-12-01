8a15bff7d437d06a4c104c9e2f77c461536a7fae
Koussaykouki <koussay.kouki@esprit.tn>
2024-12-02 00:15:19 +0100

    news and market data fetching for games

--- src/main/java/tn/zeros/marketmaster/config/SecurityConfig.java
+++ src/main/java/tn/zeros/marketmaster/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/ws-market-data/**").permitAll() // Allow WebSocket connections
+                        .requestMatchers("/ws-market-data/**").permitAll()
                         .requestMatchers("/asset/**").permitAll()
                         .anyRequest().authenticated()
                 )
--- src/main/java/tn/zeros/marketmaster/config/WebSocketConfig.java
+++ src/main/java/tn/zeros/marketmaster/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package tn.zeros.marketmaster.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,7 +10,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
+    @Value("${frontend.origin}")
+    private String frontendOrigin;
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
@@ -18,6 +20,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-market-data").setAllowedOrigins("http://localhost:4200").withSockJS();
+        registry.addEndpoint("/ws-market-data").setAllowedOrigins(frontendOrigin).withSockJS();
     }
 }
